1099bca9532dd3dba4407baec2d954cb815ad88f
Nikolaj Bjørnager Krebs <112914618+NikoBK@users.noreply.github.com>
2023-02-05 00:06:52 +0100

    Basic income/outcome/edit menu screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.IncomeButton = new System.Windows.Forms.Button();
+            this.OutcomeButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.CurrentPagePanel = new System.Windows.Forms.Panel();
+            this.okButton = new System.Windows.Forms.Button();
+            this.DueDateLabel = new System.Windows.Forms.Label();
+            this.ValueInputTextBox = new System.Windows.Forms.TextBox();
+            this.ValueLabel = new System.Windows.Forms.Label();
+            this.NameInputTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.DueDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.CurrentPageLabel = new System.Windows.Forms.Label();
+            this.PageListBox = new System.Windows.Forms.ListBox();
+            this.CurrentPagePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // IncomeButton
+            // 
+            this.IncomeButton.Location = new System.Drawing.Point(12, 12);
+            this.IncomeButton.Name = "IncomeButton";
+            this.IncomeButton.Size = new System.Drawing.Size(116, 33);
+            this.IncomeButton.TabIndex = 0;
+            this.IncomeButton.Text = "Income";
+            this.IncomeButton.UseVisualStyleBackColor = true;
+            this.IncomeButton.Click += new System.EventHandler(this.IncomeButton_Click);
+            // 
+            // OutcomeButton
+            // 
+            this.OutcomeButton.Location = new System.Drawing.Point(12, 51);
+            this.OutcomeButton.Name = "OutcomeButton";
+            this.OutcomeButton.Size = new System.Drawing.Size(116, 33);
+            this.OutcomeButton.TabIndex = 1;
+            this.OutcomeButton.Text = "Outcome";
+            this.OutcomeButton.UseVisualStyleBackColor = true;
+            this.OutcomeButton.Click += new System.EventHandler(this.OutcomeButton_Click);
+            // 
+            // EditButton
+            // 
+            this.EditButton.Enabled = false;
+            this.EditButton.Location = new System.Drawing.Point(12, 90);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(116, 33);
+            this.EditButton.TabIndex = 2;
+            this.EditButton.Text = "Edit";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // CurrentPagePanel
+            // 
+            this.CurrentPagePanel.Controls.Add(this.okButton);
+            this.CurrentPagePanel.Controls.Add(this.DueDateLabel);
+            this.CurrentPagePanel.Controls.Add(this.ValueInputTextBox);
+            this.CurrentPagePanel.Controls.Add(this.ValueLabel);
+            this.CurrentPagePanel.Controls.Add(this.NameInputTextBox);
+            this.CurrentPagePanel.Controls.Add(this.NameLabel);
+            this.CurrentPagePanel.Controls.Add(this.DueDateTimePicker);
+            this.CurrentPagePanel.Controls.Add(this.CurrentPageLabel);
+            this.CurrentPagePanel.Controls.Add(this.PageListBox);
+            this.CurrentPagePanel.Location = new System.Drawing.Point(134, 12);
+            this.CurrentPagePanel.Name = "CurrentPagePanel";
+            this.CurrentPagePanel.Size = new System.Drawing.Size(419, 286);
+            this.CurrentPagePanel.TabIndex = 3;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(341, 251);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 32);
+            this.okButton.TabIndex = 8;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // DueDateLabel
+            // 
+            this.DueDateLabel.AutoSize = true;
+            this.DueDateLabel.Location = new System.Drawing.Point(16, 96);
+            this.DueDateLabel.Name = "DueDateLabel";
+            this.DueDateLabel.Size = new System.Drawing.Size(58, 15);
+            this.DueDateLabel.TabIndex = 7;
+            this.DueDateLabel.Text = "Due Date:";
+            // 
+            // ValueInputTextBox
+            // 
+            this.ValueInputTextBox.Location = new System.Drawing.Point(245, 57);
+            this.ValueInputTextBox.Name = "ValueInputTextBox";
+            this.ValueInputTextBox.Size = new System.Drawing.Size(112, 23);
+            this.ValueInputTextBox.TabIndex = 6;
+            // 
+            // ValueLabel
+            // 
+            this.ValueLabel.AutoSize = true;
+            this.ValueLabel.Location = new System.Drawing.Point(245, 39);
+            this.ValueLabel.Name = "ValueLabel";
+            this.ValueLabel.Size = new System.Drawing.Size(38, 15);
+            this.ValueLabel.TabIndex = 5;
+            this.ValueLabel.Text = "Value:";
+            // 
+            // NameInputTextBox
+            // 
+            this.NameInputTextBox.Location = new System.Drawing.Point(16, 57);
+            this.NameInputTextBox.Name = "NameInputTextBox";
+            this.NameInputTextBox.Size = new System.Drawing.Size(161, 23);
+            this.NameInputTextBox.TabIndex = 4;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(16, 39);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(42, 15);
+            this.NameLabel.TabIndex = 3;
+            this.NameLabel.Text = "Name:";
+            // 
+            // DueDateTimePicker
+            // 
+            this.DueDateTimePicker.Location = new System.Drawing.Point(16, 114);
+            this.DueDateTimePicker.Name = "DueDateTimePicker";
+            this.DueDateTimePicker.Size = new System.Drawing.Size(200, 23);
+            this.DueDateTimePicker.TabIndex = 2;
+            // 
+            // CurrentPageLabel
+            // 
+            this.CurrentPageLabel.AutoSize = true;
+            this.CurrentPageLabel.Location = new System.Drawing.Point(3, 9);
+            this.CurrentPageLabel.Name = "CurrentPageLabel";
+            this.CurrentPageLabel.Size = new System.Drawing.Size(107, 15);
+            this.CurrentPageLabel.TabIndex = 1;
+            this.CurrentPageLabel.Text = "Current Page Label";
+            // 
+            // PageListBox
+            // 
+            this.PageListBox.FormattingEnabled = true;
+            this.PageListBox.ItemHeight = 15;
+            this.PageListBox.Location = new System.Drawing.Point(0, 27);
+            this.PageListBox.Name = "PageListBox";
+            this.PageListBox.Size = new System.Drawing.Size(419, 259);
+            this.PageListBox.TabIndex = 0;
+            this.PageListBox.SelectedIndexChanged += new System.EventHandler(this.PageListBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.CurrentPagePanel);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.OutcomeButton);
+            this.Controls.Add(this.IncomeButton);
+            this.Name = "Form1";
+            this.Text = "Economics Manager";
+            this.CurrentPagePanel.ResumeLayout(false);
+            this.CurrentPagePanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button IncomeButton;
+        private Button OutcomeButton;
+        private Button EditButton;
+        private Panel CurrentPagePanel;
+        private ListBox PageListBox;
+        private Label CurrentPageLabel;
+        private Button okButton;
+        private Label DueDateLabel;
+        private TextBox ValueInputTextBox;
+        private Label ValueLabel;
+        private TextBox NameInputTextBox;
+        private Label NameLabel;
+        private DateTimePicker DueDateTimePicker;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,86 @@ namespace economics_manager
 {
     public partial class Form1 : Form
     {
+        private bool noListBoxContent = false;
+
         public Form1()
         {
             InitializeComponent();
+            CurrentPagePanel.Visible = false;
+
+            NameLabel.Visible = false;
+            ValueLabel.Visible = false;
+            DueDateLabel.Visible = false;
+
+            NameInputTextBox.Visible = false;
+            ValueInputTextBox.Visible = false;
+            DueDateTimePicker.Visible = false;
+
+            okButton.Visible = false;
+        }
+
+        private void IncomeButton_Click(object sender, EventArgs e)
+        {
+            string[] income = { };
+            DispatchPageListBox(income, "income", true);
+        }
+
+        private void OutcomeButton_Click(object sender, EventArgs e)
+        {
+            string[] outcome = { };
+            DispatchPageListBox(outcome, "outcome", true);
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            DispatchPageListBox(null, "", false);
+            DispatchEditScreen(true);
+            EditButton.Enabled = false;
+        }
+
+        private void PageListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!noListBoxContent) {
+                EditButton.Enabled = true;
+            }
+        }
+
+        public void DispatchPageListBox(string[] content, string label, bool visible)
+        {
+            if (!CurrentPagePanel.Visible) {
+                CurrentPagePanel.Visible = true;
+            }
+
+            CurrentPageLabel.Text = $"All monthly {label}:";
+
+            PageListBox.Items.Clear();
+            PageListBox.Visible = visible;
+
+            if (content != null) {
+                PageListBox.Items.AddRange(content);
+            }
+
+            if (PageListBox.Items.Count == 0) {
+                noListBoxContent = true;
+                PageListBox.Items.Add($"No monthly {label} found...");
+            }
+        }
+
+        public void DispatchEditScreen(bool visible)
+        {
+            NameLabel.Visible       = visible;
+            ValueLabel.Visible      = visible;
+            DueDateLabel.Visible    = visible;
+            NameInputTextBox.Visible    = visible;
+            ValueInputTextBox.Visible   = visible;
+            DueDateTimePicker.Visible   = visible;
+            okButton.Visible = visible;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            DispatchEditScreen(false);
+            EditButton.Enabled = false;
         }
     }
 }
